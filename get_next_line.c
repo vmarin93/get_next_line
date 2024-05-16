ae339f7ea748d575bd815eb1e31a926f4154bc01
Vasile Marin <vasilemarin93@gmail.com>
2024-05-16 07:49:30 +0100

    finally solved it

--- get_next_line.c
+++ get_next_line.c
@@ -16,10 +16,35 @@
 #include <fcntl.h>
 
 #ifndef BUFFER_SIZE
-# define BUFFER_SIZE 14
+# define BUFFER_SIZE 1 
 #endif
 
-int	ft_strlen(char *str)
+void	shift_buffer(char **buffer, int i)
+{
+	char	*temp;
+	int	j;
+
+	j = i;
+	while((*buffer)[j] != '\0')
+	{
+		j++;
+	}
+	j = j - i;
+	temp = malloc(sizeof(char) * (j + 1));
+	if (temp == NULL)
+		return ;
+	j = 0;
+	while ((*buffer)[i + j] != '\0')
+	{
+		temp[j] = (*buffer)[i + j];
+		j++;
+	}
+	temp[j] = '\0';
+	free(*buffer);
+	*buffer = temp;
+}
+
+int	ft_strlen(const char *str)
 {
 	int	len;
 
@@ -29,27 +54,24 @@ int	ft_strlen(char *str)
 	return (len);
 }
 
-void	shift_buffer(char *buffer, int i)
+char	*ft_strchr(const char *s, int c)
 {
-	int	j;
-	int	k;
-	int	buffer_len;
-	char	*temp;
+	int		i;
+	int		len;
+	char	to_find;
 
-	buffer_len = ft_strlen(buffer);
-	temp = malloc(sizeof(char) * (buffer_len - i + 1));
-	if (temp == NULL)
-		return ;
-	j = i + 1;
-	k = 0;
-	while (buffer[j] != '\0')
+	to_find = (char)c;
+	len = ft_strlen(s);
+	i = 0;
+	while (i < len + 1)
 	{
-		temp[k] = buffer[j + k];
+		if (s[i] == to_find)
+			return ((char *)s + i);
+		i++;
 	}
-	temp[k] = '\0';
-	free (buffer);
-	buffer = temp;
+	return (NULL);
 }
+
 
 char	*get_line(char *buffer, int i)
 {
@@ -66,60 +88,92 @@ char	*get_line(char *buffer, int i)
 		j++;
 	}
 	line[j] = '\0';
-	shift_buffer(buffer, i);
 	return (line);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	src_len;
+
+	src_len = ft_strlen(src);
+	if (src_len + 1 < size)
+	{
+		i = 0;
+		while (i < src_len + 1)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+	}
+	else if (size != 0)
+	{
+		i = 0;
+		while (i < size - 1)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (src_len);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*duplicate;
+	size_t	i;
+
+	duplicate = malloc(sizeof(char) * ft_strlen(s) + 1);
+	if (duplicate == NULL)
+		return (NULL);
+	i = 0;
+	while (i < ft_strlen(s) + 1)
+	{
+		duplicate[i] = s[i];
+		i++;
+	}
+	duplicate[i] = '\0';
+	return (duplicate);
 }
 
 char	*read_from_file(int fd)
 {
 	static char	*buffer = NULL;
 	char	*temp;
+	char	*line;
+	char	*newline_address;
 	ssize_t	bytes_read;
 	int	i;
+	int	line_len;
 	
-	bytes_read = 1;
-	while (bytes_read > 0)
+	if (buffer == NULL)
 	{
+		buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 		if (buffer == NULL)
+			return (NULL);
+	}
+	i = 0;
+	while ((bytes_read = read(fd, buffer + i, BUFFER_SIZE)) >= 0)
+	{
+		i += bytes_read;
+		buffer[i] = '\0';
+		newline_address = ft_strchr(buffer, '\n');
+		if (newline_address != NULL)
 		{
-			buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-			if (buffer == NULL)
-				return (NULL);
-			bytes_read = read(fd, buffer, BUFFER_SIZE);
-			buffer[bytes_read] = '\0';
-			i = 0;
-			while (buffer[i] != '\0')
-			{
-				if (buffer[i] == '\n')
-					return (get_line(buffer, i));
-				i++;
-			}
+			line_len = newline_address - buffer;
+			line = get_line(buffer, line_len);
+			shift_buffer(&buffer, line_len + 1);
+			return (line);
 		}
 		else
 		{
-			i = 0;
-			while (buffer[i] != '\0')
-				i++;
-			temp = malloc(sizeof(char) * (i + BUFFER_SIZE + 1));
+			temp = malloc(sizeof(char) * (BUFFER_SIZE + ft_strlen(buffer) + 1));
 			if (temp == NULL)
 				return (NULL);
-			i = 0;
-			while (buffer[i] != '\0')
-			{
-				temp[i] = buffer[i];
-				i++;
-			}
-			bytes_read = read(fd, temp + i, BUFFER_SIZE);
-			temp[bytes_read + i] = '\0';
-			free (buffer);
+			ft_strlcpy(temp, buffer, BUFFER_SIZE + ft_strlen(buffer) + 1);
+			free(buffer);
 			buffer = temp;
-			i = 0;
-			while (buffer[i] != '\0')
-			{
-				if (buffer[i] == '\n')
-					return (get_line(buffer, i));
-				i++;
-			}
 		}
 	}
 	return (NULL);
@@ -129,8 +183,7 @@ char	*get_next_line(int fd)
 {
 	char	*line;
 
-	line = NULL;
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, line, 0) < 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	line = read_from_file(fd);
 	return (line);
@@ -139,12 +192,17 @@ char	*get_next_line(int fd)
 int	main(void)
 {
 	int	fd;
+	int	fd2;
 	char	*line;
 
 	fd = open("file.txt", O_RDONLY);
+	fd2 = open("file2.txt", O_RDONLY);
 
 	printf("first call ->\n");
 	line = get_next_line(fd);
+	printf("%s\n", line);
+	printf("first call second file ->\n");
+	line = get_next_line(fd2);
 	printf("%s\n", line);
 	printf("second call ->\n");
 	line = get_next_line(fd);
@@ -152,6 +210,8 @@ int	main(void)
 	printf("third call ->\n");
 	line = get_next_line(fd);
 	printf("%s\n", line);
-	free (line);
+	printf("third call ->\n");
+	line = get_next_line(fd);
+	printf("%s\n", line);
 	close (fd);
 }
